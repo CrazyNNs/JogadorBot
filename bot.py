98ddc3e25a0e5ea098ebbef483358f171e662fb6
CrazyNNs <156045572+CrazyNNs@users.noreply.github.com>
2026-07-10 22:12:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 from PIL import Image, ImageDraw, ImageFont
 from zoneinfo import ZoneInfo
+from discord import ui
 import random
 import datetime
 import sqlite3
