00b6fab45f2ef488ce541ed8582752ca7b16f944
CrazyNNs <156045572+CrazyNNs@users.noreply.github.com>
2026-08-06 04:34:06 -0300

    Atualizar o bot.py

--- bot.py
+++ bot.py
@@ -2492,7 +2492,7 @@ class ViewItensMineracao(discord.ui.View):
         itens = {nome: dados for nome, dados in ITENS_MINERACAO.items() if dados["subcategoria"] == self.subcategoria}
         for nome, dados in itens.items():
             embed.add_field(
-                name=f"{dados['emoji']} {nome} — {dados['preco']} {nome_moeda}",
+                name=f"{dados['emoji']} {nome} — {dados['preco']} {dados['nome_moeda']}",
                 value=dados["descricao"],
                 inline=False
             )
