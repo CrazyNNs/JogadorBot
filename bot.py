2e21307630914086480dd0fa576c60751038dbc3
CrazyNNs <156045572+CrazyNNs@users.noreply.github.com>
2026-09-12 20:11:58 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -768,7 +768,7 @@ INVENTARIO_ESTRUTURA = {
 DOUBLE_FATIAS = (
     [{"cor": "vermelho", "emoji": "🟥"}] * 7 +
     [{"cor": "preto", "emoji": "⬛"}] * 7 +
-    [{"cor": "branco", "emoji": "⬜"}]
+    [{"cor": "branco", "emoji": "⬜"}] * 7 +
 )
 
 DOUBLE_MULTIPLICADORES = {
